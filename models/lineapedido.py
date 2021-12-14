f2fa8b4431700f7600f89e283d26c40ff630fa14
antonio <dwccdiw@gmail.com>
2021-12-14 10:41:07 +0100

    Commit many2many

--- models/lineapedido.py
+++ models/lineapedido.py
@@ -13,3 +13,8 @@ class lineapedido(models.Model):
      descripcion_da_lineapedido = fields.Text(string="A descripción")
      # Os campos Many2one crean un campo na BD
      pedido_id = fields.Many2one('odoo_basico.pedido',ondelete="cascade", required=True)
+     # Os campos Many2many crean unha táboa na BD
+     informacion_ids = fields.Many2many("odoo_basico.informacion",
+                                        string="Rexistro de Información",
+                                        relation="odoo_basico_lineapedido_informacion",
+                                        column1="lineapedido_id", column2="informacion_id")
